3fcbba6863efdd0f63980bb287d72fe784063606
Dmitriy Danilin <danilindn@icloud.com>
2020-07-25 16:12:04 +0300

    доп

---
+++ UIPageView/ContentViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ContentViewController.swift
+//  UIPageView
+//
+//  Created by Дмитрий Данилин on 25.07.2020.
+//  Copyright © 2020 Дмитрий Данилин. All rights reserved.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+    
+    @IBOutlet weak var descriptionHelp: UILabel!
+    @IBOutlet weak var emojiSymbol: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var presentText = ""
+    var emoji = ""
+    var currentPage = 0 // Номер текущей страницы
+    var numberOfPage = 0 // Количество страниц
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        descriptionHelp.text = presentText
+        emojiSymbol.text = emoji
+        pageControl.currentPage = currentPage
+        pageControl.numberOfPages = numberOfPage 
+
+        // Do any additional setup after loading the view.
+    }
+}
